19d128e883f27ec10e0dea77473e1ff6bcdfdbcc
Jaswanth <jaswanth.work20@gmail.com>
2026-09-18 11:55:10 +0000

    second commit

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/api/hello")
 def hello():
     return jsonify({
-        "message": "Hello Jaswanth Kumar Kamgundhi How are you rey!",
+        "message": "Hello Jaswanth Kumar Kamgundhi How are you doing?",
         "status": "success"
     })
 
