2250a85935444eac5ce9591353467f9a2d4ed1e5
Jaswanth <jaswanth.work20@gmail.com>
2026-09-22 04:54:52 +0000

    run cicd

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/api/hello")
 def hello():
     return jsonify({
-        "message": "Hey JASWANTH, How are you doing?Im doing good",
+        "message": "Hey Kumar, How are you doing?Im doing good",
         "status": "success"
     })
 
