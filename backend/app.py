3498fd2ea82bb80ad0570194ec74eca4193ef75e
Jaswanth <jaswanth.work20@gmail.com>
2026-09-22 05:36:56 +0000

    run

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     return jsonify(
         {
-            "message": "Hey Jaswanth, How are you doing? Im doing good",
+            "message": "Hey Jaswanth Kumar, How are you doing? Im doing good",
             "status": "success",
         }
     )
