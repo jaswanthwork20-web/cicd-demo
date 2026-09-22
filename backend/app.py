ac29a647a1383e90ec96d1162457be440cbafe5c
Jaswanth <jaswanth.work20@gmail.com>
2026-09-22 06:34:19 +0000

    run cicd

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     return jsonify(
         {
-            "message": "Hey  , How are you doing? Im doing good",
+            "message": "Hey jASWANTH  , How are you doing? Im doing good",
             "status": "success",
         }
     )
